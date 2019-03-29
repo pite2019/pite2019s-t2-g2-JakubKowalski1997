8ae6515956e77112b65828e70174755718eb0d5a
JakubKowalski1997 <kubabartek1@op.pl>
2019-03-29 14:16:10 +0100

    add new features

--- Bank.py
+++ Bank.py
@@ -11,10 +11,18 @@ class Bank:
     def add_client(self, client):
         self.list_of_clients.append(client)
 
+    def input_cash(self, client, money):
+        self.check_if_client_exists(client)
+        client.money += money
+
+    def withdraw_cash(self, client, money):
+        self.check_if_client_exists(client)
+        client.money -= money 
+
     def check_if_client_exists(self, *clients_to_check):
         for client in clients_to_check:
             if client in self.list_of_clients:
-                return
+                return True
             else:
                 raise Exception("This client dont exists in bank")
     
--- task.py
+++ task.py
@@ -32,8 +32,21 @@ def main():
     client1 = Client(100)
     client2 = Client(200)
     bank = Bank("Alior")
-
+    bank.add_client(client1)
+    bank.add_client(client2)
+    print("First client money: {}".format(client1.money))
+    print("Second client money: {}".format(client2.money))
     bank.transfer_money(client1, client2, 100)
+    print("First client money: {}".format(client1.money))
+    print("Second client money: {}".format(client2.money))
+    
+    print("First client money: {}".format(client1.money))
+    bank.input_cash(client1, 100)
+    print("First client money: {}".format(client1.money))
+
+    print("Second client money: {}".format(client2.money))
+    bank.withdraw_cash(client2, 100)
+    print("Second client money: {}".format(client2.money))
 
 
 if __name__ == "__main__":
